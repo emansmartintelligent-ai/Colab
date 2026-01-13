0b4f78f7c8308dbe649d10139badda33eaed19a0
emansmartintelligent-ai <emansmartintelligent@gmail.com>
2026-01-13 23:23:19 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'sobel-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const ASSETS = ['./Sobeler-index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
